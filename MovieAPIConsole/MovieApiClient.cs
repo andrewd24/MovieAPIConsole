b6e58303c14fe1e6f21b004abab926e48d44fbef
andrewd24 <andrew.douglas@eagles.usm.edu>
2015-04-15 15:18:29 -0500

    RottenTomatoesAPI
    C# console app that hits Rotten Tomatoes API and returns movie data.

---
+++ MovieAPIConsole/MovieApiClient.cs
@@ -0,0 +1,51 @@
+﻿/*-----------------------------------------------------------------------------
+ *MovieAPIClient.cs
+ * 
+ *Hits the Rotten Tomatoes API and catches the JSON response.  Passes the JSON 
+ *to the MovieData class for serialization.
+ ----------------------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace MovieConsole
+{
+    public static class MovieApiClient
+    {
+        public static void GetMovieInfo()
+        {
+            var url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=a677j8k8uajxetap8rvgu7kq&q="+ Program.movieTitle + "&page_limit=1"; //url to pull the weather data from
+
+            var syncClient = new WebClient();                                                         
+            var content = syncClient.DownloadString(url);
+            
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(MovieData));
+            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(content)))
+            {
+                var movieInfo = (MovieData)serializer.ReadObject(ms);
+               
+               Program.movieTitle = movieInfo.movies[0].title;                             //sets the movie title
+               Program.year = movieInfo.movies[0].year;                                    //sets the year
+               Program.MPAArating = movieInfo.movies[0].mpaa_rating;                       //motion picture rating
+               Program.criticScore = movieInfo.movies[0].ratings.critics_score;            //critic numerical score
+               Program.criticRating = movieInfo.movies[0].ratings.critics_rating;          //critic text score
+               Program.audienceScore = movieInfo.movies[0].ratings.audience_score;         //audience numerical score
+               Program.audienceRating = movieInfo.movies[0].ratings.audience_rating;       //audience text score
+               Program.synopsis = movieInfo.movies[0].synopsis;                            //short synopsis if available  
+               Program.cast1 = movieInfo.movies[0].abridged_cast[0].name;                  //first cast member
+               Program.cast2 = movieInfo.movies[0].abridged_cast[1].name;                  //second cast member
+               Program.cast3 = movieInfo.movies[0].abridged_cast[2].name;                  //third cast member
+              
+               
+                }
+            }
+            
+        }
+    }
+
---
+++ MovieAPIConsole/Program.cs
@@ -0,0 +1,68 @@
+﻿/*-----------------------------------------------------------------------------
+ Programmer:  Andrew Douglas
+ 
+ Program:     Rotten Tomatoes API - Hits the Rotten Tomatoes API after prompting
+              the user for a movie title.  Then parses the JSON response and 
+              creates a HTML file containing the parsed response.
+  
+ Course:      CSS 405 - Web Programming
+ 
+ Instructor:  Tom Rishel
+ -----------------------------------------------------------------------------*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace MovieConsole
+{
+    class Program
+    {
+        
+        public static string movieTitle;                  //declaration of variable for the program
+        public static int year; 
+        public static string MPAArating;
+        public static int criticScore;     
+        public static string criticRating;
+        public static int audienceScore;
+        public static string audienceRating;
+        public static string synopsis;
+        public static string cast1;
+        public static string cast2;
+        public static string cast3;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a movie title");             //prompt for movie title
+            movieTitle = Console.ReadLine();                             //store in movie title
+            movieTitle = movieTitle.Replace(' ', '+');             //convert blank spaces in title to plus signs   
+            
+           
+             
+            MovieApiClient.GetMovieInfo();
+
+            //header of the html document
+            string header = "<html><head><link href=\"movies.css\" rel=\"stylesheet\" type=\"text/css\" />" + "</head><body><h1>" + movieTitle + "</h1><table>" +
+                "<colgroup><col class=\"info\" span=\"6\"/><thead><tr><th>Year</th><th>Rating</th><th>Critic Score</th><th>Critic Rating</th><th>Audience Score</th><th>Audience Rating</th>"+ 
+                "</tr>";
+            //body fo the html document
+            string body = "<td>" + year + "</td><td>" + MPAArating + "</td><td>" + criticScore +"</td><td>" + criticRating + "</td>" +
+                "<td>" + audienceScore + "</td><td>" + audienceRating + "</td> </table>";
+            //cast inserted into the document
+            string cast = "<h2>Starring:</h2><div id=\"cast\"><br>" + cast1 + "<br>" + cast2 + "<br>" + cast3 +"</div>";
+            //end of the html
+            string tail = "<p>" + synopsis + "</p></body></html>";
+            //adds all of the html together
+            string everything = header + body + cast + tail;
+            //prints the html into a file called movieData
+            using (StreamWriter outfile = new StreamWriter(@"C:\Users\Andrew\Documents\movieData.htm"))
+            {
+                outfile.Write(everything);
+            }
+
+
+    
+        }
+    }
+}
